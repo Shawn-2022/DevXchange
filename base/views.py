6d45b088a7297fd575eb4f314aab375fcf208e7c
Shawn <imjoshishyam@gmail.com>
2023-04-10 14:01:18 +0530

    model building

--- base/views.py
+++ base/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id':1,'name':'Lets go boi'},
+]
 
 def home(request):
-    return render (request, 'home.html')
+    context = {'rooms':rooms}
+    return render (request, 'base/home.html',context)
 
 
-def room(request):
-    return render (request, 'home.html')
+def room(request,id):
+    room = None
+    for i in rooms:
+        if i['id'] == int(id):
+            room = i
+    context = {'room':room}
+    return render (request, 'base/room.html',context)
